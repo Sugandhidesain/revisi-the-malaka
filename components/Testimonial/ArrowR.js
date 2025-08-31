e816763e901104391046d4f408e920485de2ab7c
Muhamad Sugandhi Ramadhan <muhamadsugandhi97@gmail.com>
2025-08-31 20:17:59 +0700

    Add files via upload

---
+++ components/Testimonial/ArrowR.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function ArrowR() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="none"
+      viewBox="0 0 20 20"
+    >
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4.167 10h11.666M10 4.167L15.833 10 10 15.834"
+      ></path>
+    </svg>
+  );
+}
+
+export default ArrowR;
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import SectionBeli from "@/components/Beli/SectionBeli";
+import Footer from "@/components/Footer/Footer";
+import SectionHarga from "@/components/Harga/SectionHarga";
+import Header from "@/components/Header";
+import SectionManfaat from "@/components/Manfaat/SectionManfaat";
+import SectionPengantar from "@/components/Pengantar/SectionPengantar";
+import SectionPenulis from "@/components/Penulis/SectionPenulis";
+import SectionPratinjau from "@/components/Pratinjau/SectionPratinjau";
+import SectionSelain from "@/components/Selain/SectionSelain";
+import SectionSiapa from "@/components/Siapa/SectionSiapa";
+import SectionTestimonial from "@/components/Testimonial/SectionTestimonial";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <SectionSiapa />
+      <SectionPengantar />
+      <SectionManfaat />
+      <SectionSelain />
+      <SectionPratinjau />
+      <SectionHarga />
+      <SectionPenulis />
+      <SectionTestimonial />
+      <SectionBeli />
+      <Footer />
+    </>
+  );
+}
